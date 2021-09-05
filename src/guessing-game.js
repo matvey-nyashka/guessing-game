55154506f355bfd916123464584a71a25e2030d4
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-09-06 00:35:29 +0300

    feat: added solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.av = 0
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.av = Math.ceil((this.max + this.min) / 2);
+        return this.av
     }
 
     lower() {
-
+        this.max = this.av;
     }
 
     greater() {
-
+        this.min = this.av;
     }
 }
 
